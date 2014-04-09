5b4f5f2f0dda36e96ba52222dba4ea6d5e3ceee1
Christina Kim <christinahkim1@gmail.com>
2014-04-08 20:56:57 -0500

    edited php email contact us form

--- php/config.php
+++ php/config.php
@@ -6,4 +6,4 @@
 
 // This will be where the emails for the contact form will be sent
 
-$config['contact_email'] = "example@example.com";
+$config['contact_email'] = "asg-academic@u.northwestern.edu";
