8bf19823470d1fd022933ff4df560c745185a726
Sheng Wu <pkernoobie@gmail.com>
2014-04-26 23:47:06 -0500

    yup

--- vote/index.php
+++ vote/index.php
@@ -2,6 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Vote for the RedesigNU Students' Choice Awards winners!</title>
     <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css" />
     <style type="text/css">
